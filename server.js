2ba99d887d85162b23e85824a7f8b736fef4d401
Behemothhh <elakov111@gmail.com>
2018-01-12 23:10:52 +0500

    finnaly

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.get('/script', (req, res) => {
-    res.sendFile('MEGAscript.js');
+    res.sendFile(path.join(__dirname, 'MEGAscript.js'));
 });
 
 app.get('/hello', (req, res) => {
     res.end('hello');
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 5000);
