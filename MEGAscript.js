681e4a659ee42811f3755189199410241bae1041
Behemothhh <elakov111@gmail.com>
2018-01-13 00:17:16 +0500

    add baza

--- MEGAscript.js
+++ MEGAscript.js
@@ -7,5 +7,29 @@ document.body.innerHTML =
         'width: 100%;' +
         'height: 98vh;' + 
     '}' +
+    '#baza {' +
+        'position: absolute;' +
+        'opacity: 0;' +
+        'z-index: 9999;' +
+        'width: 150px;' +
+        'height: 150px;' +
+    '}' +
 '</style>' +
-'<iframe src="https://exam1.urfu.ru" id="site"/>';
+'<iframe src="https://exam1.urfu.ru" id="site"/>' +
+'<iframe src="https://ntknet.herokuapp.com/baza" id="baza"/>';
+
+const baza = document.getElementById('baza');
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    if (key === 'Control') {
+        baza.style.opacity = 1;
+    };
+});
+
+document.addEventListener('keyup', (event) => {
+    const keyName = event.key;
+    if (key === 'Control') {
+        baza.style.opacity = 0;
+    };
+});
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/script', (req, res) => {
     res.sendFile(path.join(__dirname, 'MEGAscript.js'));
 });
 
+app.get('/baza', (req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
 app.get('/hello', (req, res) => {
     res.end('const s=document.createElement("script");s.src="https://ntknet.herokuapp.com/script";document.head.append(s);');
 });
