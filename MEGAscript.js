d7844954701c0fcc418665545b35df5ec7d87613
Behemothhh <elakov111@gmail.com>
2018-01-12 23:26:25 +0500

    test

--- MEGAscript.js
+++ MEGAscript.js
@@ -1,1 +1,8 @@
-document.body.innerHTML = 'HELLO FROM JHON';
+document.body.innerHTML =
+'<style>' +
+    '#site {' +
+        'width: 100%;' +
+        'height: 100vh;' + 
+    '}' +
+'</style>' +
+'<iframe src="exam1.urfu.ru" id="site"/>';
