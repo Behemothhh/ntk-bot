8a7ccb164873d7ea127d519ce5665e2c9716890c
Behemothhh <elakov111@gmail.com>
2018-01-24 09:19:16 +0500

    added eco

---
+++ scripts/eco.js
@@ -0,0 +1,88 @@
+document.body.innerHTML =
+'<style>' +
+    '#site {' +
+        'border: 0;' +
+        'background-color: #fff !important;' +
+        'position: absolute;' +
+        'top: -2px;' +
+        'left: 0;' +
+        'width: 100%;' +
+        'height: 98vh;' + 
+    '}' +
+    '#baza {' +
+        'border: 0;' +
+        'background-color: #fff !important;' +
+        'position: absolute;' +
+        'opacity: 0;' +
+        'z-index: -1;' +
+        'width: 500px;' +
+        'height: 300px;' +
+        'bottom: 0' +
+    '}' +
+    '#settings {' +
+        'background-color: #fff !important;' +
+        'position: absolute;' +
+        'opacity: 0;' +
+        'z-index: -2;' +
+        'bottom: 0;' +
+        'right: 0;' +
+    '}' +
+'</style>' +
+'<iframe src="https://exam1.urfu.ru" id="site"></iframe>' +
+'<div id="settings">' +
+    '<input type="number" id="width" value="500" placeholder="Width" step="10">' +
+    '<input type="number" id="height" value="300" placeholder="Height" step="10">' +
+    '<input type="number" id="bottom" value="0" placeholder="Bottom" step="5">' +
+    '<input type="number" id="left" value="0" placeholder="Left" step="5">' +
+'</div>' +
+'<iframe src="https://ntknet.herokuapp.com/baza/eco.html" id="baza"></iframe>';
+
+const baza = document.getElementById('baza');
+let pressed = {};
+let a = Object.assign({}, document.getElementById('site').contentWindow);
+let switcher = true;
+
+document.getElementById('site').addEventListener('load', () => {
+    document.getElementById('site').contentWindow.document.addEventListener('keydown', (event) => {
+        pressed[event.code] = true;
+        if (pressed['AltLeft'] && pressed['KeyZ']) {
+            console.log('press on site');
+            baza.style.opacity = 1;
+            baza.style.zIndex = 9999;
+        }
+        if (pressed['AltLeft'] && pressed['KeyX']) {
+            toggleSettings();
+            delete pressed['AltLeft'];
+            delete pressed['KeyX'];
+        }
+    });
+    
+    document.getElementById('site').contentWindow.document.addEventListener('keyup', (event) => {
+        baza.style.opacity = 0;
+        baza.style.zIndex = -1;
+        delete pressed[event.code];
+    });
+
+    [].slice.call(document.getElementById('settings').children).forEach((child) => {
+        console.log('added');
+        child.addEventListener('change', (e) => {
+            console.log(e);
+            document.getElementById('baza').style[e.target.id] = e.target.value + 'px';
+        });
+    });
+
+});
+
+function toggleSettings() {
+    if (switcher) {
+        let s = document.getElementById('settings');
+        s.style.zIndex = 99999;
+        s.style.opacity = 1;
+        switcher = false;
+    } else {
+        let s = document.getElementById('settings');
+        s.style.zIndex = -2;
+        s.style.opacity = 0;
+        switcher = true;
+    }
+}
--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/script', (req, res) => {
-    res.sendFile(path.join(__dirname, 'MEGAscript.js'));
-});
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
 
-app.use('/baza', express.static(path.join(__dirname, 'pages')))
+app.use('/baza', express.static(path.join(__dirname, 'pages')));
 
 app.get('/', (req, res) => {
-    res.end('const s=document.createElement("script");s.src="https://ntknet.herokuapp.com/script";document.head.append(s);');
+    res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
